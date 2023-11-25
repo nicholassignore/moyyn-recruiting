d278da11a384a7633852bab765d51dc2439bd516
Nicholas Signore <nicholas.santos@live.de>
2023-11-25 10:47:32 +0100

    added score function

--- src/main/java/com/moyyn/recruiting/chatgpt/beans/ChatGptConfig.java
+++ src/main/java/com/moyyn/recruiting/chatgpt/beans/ChatGptConfig.java
@@ -5,7 +5,7 @@ public class ChatGptConfig {
 
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
-    public static final String API_KEY = "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";
+    public static final String API_KEY = "";
     public static final String MODEL = "text-davinci-003";
     public static final Integer MAX_TOKEN = 300;
     public static final Double TEMPERATURE = 0.0;
